b61b252388d727d75d9d1443ac99e0cd21d9edc9
Ricky DeFazio <rickysdefazio@gmail.com>
2023-04-19 22:01:04 -0400

    Adjust not found alert

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -25,23 +25,8 @@ export default function Home() {
       )}
 
       {!isLoading && notFound && (
-        <div className='alert bg-primary shadow-lg w-1/2'>
-          <div>
-            <svg
-              xmlns='http://www.w3.org/2000/svg'
-              className='stroke-current flex-shrink-0 h-6 w-6'
-              fill='none'
-              viewBox='0 0 24 24'
-            >
-              <path
-                strokeLinecap='round'
-                strokeLinejoin='round'
-                strokeWidth='2'
-                d='M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z'
-              />
-            </svg>
-            <span>No user found. Please try again.</span>
-          </div>
+        <div className='alert bg-primary shadow-lg lg:w-1/2 justify-center'>
+          No user found. Please try again. 🤞🏻
         </div>
       )}
     </main>
