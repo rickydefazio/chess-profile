241f0f3b77234edf7b8ef7e4e2ba642813cf8e63
Ricky DeFazio <rickysdefazio@gmail.com>
2023-04-20 17:51:04 -0400

    add missing return LOL

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,9 +3,10 @@ import '@/styles/globals.css';
 import type { AppProps } from 'next/app';
 
 export default function App({ Component, pageProps }: AppProps) {
-  <>
-    <Component {...pageProps} />
-
-    <Analytics />
-  </>;
+  return (
+    <>
+      <Component {...pageProps} />
+      <Analytics />
+    </>
+  );
 }
