dbc6adc37a1cc0444a80f206b62fd56e29f1b4ee
Ricky DeFazio <rickysdefazio@gmail.com>
2023-09-12 22:37:45 -0400

    fix terminal warning for metatag

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -5,7 +5,6 @@ export default function Document() {
     <Html lang='en' data-theme='dracula'>
       <Head>
         <meta charSet='utf-8' />
-        <meta name='viewport' content='width=device-width, initial-scale=1' />
         <meta
           name='description'
           content='Chess profile created from chess.com data'
