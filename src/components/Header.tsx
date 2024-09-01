7d75b26d75a2451ad5f7f7a1cb673977493a9cbe
Ricky DeFazio <rickysdefazio@gmail.com>
2024-09-01 11:38:11 -0400

    remove whitespace

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,7 @@
 export default function Header() {
   return (
     <header className='max-w-md'>
-      <h1 className='text-5xl font-bold'> Chess Profile ♟️</h1>
+      <h1 className='text-5xl font-bold'>Chess Profile ♟️</h1>
       <p className='py-6'>Search for your friends and see their stats!</p>
     </header>
   );
