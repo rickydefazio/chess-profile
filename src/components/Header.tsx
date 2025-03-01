8cce151dc621957e371fee17fc252b841c4377b3
Ricky DeFazio <rickysdefazio@gmail.com>
2025-03-01 16:11:54 -0500

    Add 'chess.com' callout for clarity.

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,7 +2,9 @@ export default function Header() {
   return (
     <header className='max-w-md'>
       <h1 className='text-5xl font-bold'>Chess Profile ♟️</h1>
-      <p className='py-6'>Search for your friends and see their stats!</p>
+      <p className='py-6'>
+        Search for your <strong>Chess.com</strong> friends and see their stats!
+      </p>
     </header>
   );
 }
